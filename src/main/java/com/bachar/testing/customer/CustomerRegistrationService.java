874ed985a9a4936be027d91d80000000c2e09648
bachardaowd <bachar.daowd@gmail.com>
2024-07-11 19:27:39 +0200

    service layer 1

--- src/main/java/com/bachar/testing/customer/CustomerRegistrationService.java
+++ src/main/java/com/bachar/testing/customer/CustomerRegistrationService.java
@@ -3,26 +3,29 @@ package com.bachar.testing.customer;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 @Service
 @AllArgsConstructor
 public class CustomerRegistrationService {
 
     private final CustomerRepository repository;
 
-//    public void registerNewCustomer(CustomerRegistrationRequest request) {
-//        if (repository.existsCustomerByPhoneNumber(request.phoneNumber())) {
-//            throw new IllegalArgumentException("Customer already exists with phone number: " + request.phoneNumber());
-//        }
-//        Customer customer = Customer.builder().name(request.name()).phoneNumber(request.phoneNumber()).build();
-//        repository.save(customer);
-//    }
     public void registerNewCustomer(CustomerRegistrationRequest request) {
         //1. Check if PhoneNumber is taken
-        //2. if taken lets check if belongs to same customer
-        //2.1 if yes return
-        //2.2 throw an exception
+        String phoneNumber = request.phoneNumber();
+        Optional<Customer> customerOptional = repository.findCustomerByPhoneNumber(phoneNumber);
+        if (customerOptional.isPresent()) {
+          //  if taken lets check if belongs to same customer
+            Customer customer = customerOptional.get();
+            if(customer.getName().equals(request.name())) {
+                return;
+            }
+            throw new IllegalStateException(String.format("Phone number %s already taken", request.phoneNumber()));
+        }
+        Customer customer = Customer.builder().name(request.name()).phoneNumber(phoneNumber).build();
         //3. Save customer
-
+        repository.save(customer);
     }
 
 }
--- src/test/java/com/bachar/testing/customer/CustomerRegistrationServiceTest.java
+++ src/test/java/com/bachar/testing/customer/CustomerRegistrationServiceTest.java
@@ -1,66 +1,66 @@
-//package com.bachar.testing.customer;
-//
-//import org.junit.jupiter.api.AfterEach;
-//import org.junit.jupiter.api.BeforeEach;
-//import org.junit.jupiter.api.Test;
-//import org.mockito.ArgumentCaptor;
-//import org.mockito.Mock;
-//import org.mockito.MockitoAnnotations;
-//
-//import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
-//import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
-//import static org.mockito.ArgumentMatchers.any;
-//import static org.mockito.BDDMockito.then;
-//import static org.mockito.Mockito.never;
-//import static org.mockito.Mockito.when;
-//
-//class CustomerRegistrationServiceTest {
-//
-//    @Mock
-//    private CustomerRepository repository;
-//    private CustomerRegistrationService underTest;
-//    private AutoCloseable autoCloseable;
-//
-//
-//    @BeforeEach
-//    void setUp() {
-//        autoCloseable = MockitoAnnotations.openMocks(this);
-//        underTest = new CustomerRegistrationService(repository);
-//    }
-//
-//    @AfterEach
-//    void tearDown() throws Exception {
-//        autoCloseable.close();
-//    }
-//
-//    @Test
-//    void registerNewCustomerHappyPath() {
-//        //Given
-//        CustomerRegistrationRequest request = CustomerRegistrationRequest.builder()
-//                .name("bachar").phoneNumber("123456").build();
-//        when(repository.existsCustomerByPhoneNumber(request.phoneNumber())).thenReturn(false);
-//        //when
-//        underTest.registerNewCustomer(request);
-//        //Then
-//        ArgumentCaptor<Customer> argumentCaptor = ArgumentCaptor.forClass(Customer.class);
-//        then(repository).should().save(argumentCaptor.capture());
-//        Customer customer = argumentCaptor.getValue();
-//        assertThat(customer.getName()).isEqualTo(request.name());
-//        assertThat(customer.getPhoneNumber()).isEqualTo(request.phoneNumber());
-//    }
-//
-//    @Test
-//    void registerNewCustomerThrowsException() {
-//        //Given
-//        CustomerRegistrationRequest request = CustomerRegistrationRequest.builder()
-//                .name("bachar").phoneNumber("123456").build();
-//        when(repository.existsCustomerByPhoneNumber(request.phoneNumber())).thenReturn(true);
-//        //Then
-////        then(repository).shouldHaveNoMoreInteractions();
-//        assertThatThrownBy(() -> underTest.registerNewCustomer(request))
-//                .isInstanceOf(IllegalArgumentException.class)
-//                .hasMessageContaining("Customer already exists with phone number:");
-//        then(repository).should(never()).save(any(Customer.class));
-//
-//    }
-//}
+package com.bachar.testing.customer;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.mockito.junit.jupiter.MockitoExtension;
+import java.util.Optional;
+
+import static org.assertj.core.api.BDDAssertions.then;
+import static org.mockito.ArgumentMatchers.any;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.*;
+
+
+@ExtendWith(MockitoExtension.class)
+class CustomerRegistrationServiceTest {
+
+
+    @Mock
+    private CustomerRepository customerRepository;
+
+    private CustomerRegistrationService underTest;
+    private AutoCloseable autoCloseable;
+
+
+
+
+    @BeforeEach
+    void setUp() {
+        autoCloseable = MockitoAnnotations.openMocks(this);
+        underTest = new CustomerRegistrationService(customerRepository);
+    }
+
+    @Test
+    void itShouldSaveNewCustomer() {
+        //Given
+        CustomerRegistrationRequest request = CustomerRegistrationRequest.builder().name("Bachar").phoneNumber("123456").build();
+        ArgumentCaptor<Customer> argumentCaptor = ArgumentCaptor.forClass(Customer.class);
+
+        when(customerRepository.findCustomerByPhoneNumber(request.phoneNumber())).thenReturn(Optional.empty());
+        //When
+        underTest.registerNewCustomer(request);
+        //Then
+        verify(customerRepository).save(argumentCaptor.capture());
+        Customer customer = argumentCaptor.getValue();
+        assertThat(customer.getName()).isEqualTo("Bachar");
+        assertThat(customer.getPhoneNumber()).isEqualTo("123456");
+    }
+
+    @Test
+    void itShouldNotSaveCustomerWhenCustomerExists() {
+        //Given
+        CustomerRegistrationRequest request = CustomerRegistrationRequest.builder().name("Bachar").phoneNumber("123456").build();
+        Customer customer = Customer.builder().name("Bachar").phoneNumber("123456").build();
+        when(customerRepository.findCustomerByPhoneNumber(request.phoneNumber())).thenReturn(Optional.of(customer));
+        //When
+        underTest.registerNewCustomer(request);
+        //Then
+//        verify(customerRepository).findCustomerByPhoneNumber(request.phoneNumber());
+        verify(customerRepository, never()).save(any(Customer.class));
+//        verifyNoMoreInteractions(customerRepository);
+    }
+}
